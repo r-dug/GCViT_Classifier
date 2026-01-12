d415e1ddfb68ef94c7e9726aa204dfc2d46e0693
Richard Douglas <80994012+r-dug@users.noreply.github.com>
2026-01-12 10:57:19 -0500

    Refactor imports and define __all__ in __init__.py
    Updated import statements to include full module paths and added __all__ for public API.

--- gcvit/layers/__init__.py
+++ gcvit/layers/__init__.py
@@ -1,9 +1,24 @@
-from .attention import WindowAttention
-from .block import Block
-from .embedding import PatchEmbed
-from .feature import SqueezeAndExcitation
-from .feature import ReduceSize
-from .feature import MLP
-from .feature import GlobalQueryGenerator
-from .feature import FeatureExtraction
-from .level import Level
+# gcvit/layers/__init__.py
+from gcvit.layers.attention import WindowAttention
+from gcvit.layers.block import Block
+from gcvit.layers.embedding import PatchEmbed
+from gcvit.layers.feature import (
+    SqueezeAndExcitation,
+    ReduceSize,
+    MLP,
+    GlobalQueryGenerator,
+    FeatureExtraction,
+)
+from gcvit.layers.level import Level
+
+__all__ = [
+    "WindowAttention",
+    "Block",
+    "PatchEmbed",
+    "SqueezeAndExcitation",
+    "ReduceSize",
+    "MLP",
+    "GlobalQueryGenerator",
+    "FeatureExtraction",
+    "Level",
+]
